dd7152cddc86930c8be16dfa3f2390850f385497
Paul <64870518+woodcock3@users.noreply.github.com>
2021-09-27 05:12:26 +0100

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+// postcss.config.js
+
+const autoprefixer = require('autoprefixer');
+const cssnanoConfig = {
+    autoprefixer: false,
+    discardComments: {removeAll: true},
+    svgo: true,
+};
+const cssnano = require('cssnano')({
+  preset: ['default', { cssnanoConfig }
+  ]
+});
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./**/*.html', './**/site.js'],
+  safelist: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track']
+});
+
+module.exports = {
+  plugins: [
+    autoprefixer,
+    ...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : [])
+  ],
+};
