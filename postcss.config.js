fba4d44899d6266da4b588827212e2a5cb454a86
Paul <64870518+woodcock3@users.noreply.github.com>
2021-11-01 04:22:41 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,14 +4,14 @@ const autoprefixer = require('autoprefixer');
 const cssnanoConfig = {
     autoprefixer: false,
     discardComments: {removeAll: true},
-    svgo: true,
+    svgo: true
 };
 const cssnano = require('cssnano')({
   preset: ['default', { cssnanoConfig }
   ]
 });
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['index.html', './**/*.html', '**/*.html', './**/site.js'],
+  content: ['index.html', './**/*.html', '**/*.html', '/**/*.html', './**/site.js'],
   css: ['assets/css/city.css'],
   safelist: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track'],
   rejected: true
