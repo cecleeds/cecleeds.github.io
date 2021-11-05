dd3c7e64d1b2555430d0e5a1c663fa3ca531dd88
Paul <64870518+woodcock3@users.noreply.github.com>
2021-11-05 05:16:49 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -10,10 +10,7 @@ const cssnano = require('cssnano')({
   preset: ['default', { cssnanoConfig }]
 });
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['*.html', '*.js'],
-  // Treat every word in the bundle as a CSS selector
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  css: ['assets/css/city.css'],
+  content: ['**/*.html', '**/*.js'],
   safelist: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track']
 });
 
