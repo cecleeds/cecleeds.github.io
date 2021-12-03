90371708ee01903af6c938a02b14c3def6d368cf
Paul <64870518+woodcock3@users.noreply.github.com>
2021-12-03 16:16:47 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -10,16 +10,10 @@ const cssnano = require('cssnano')({
   preset: ['default', { cssnanoConfig }]
 });
 
-const purgeHTML = require('purgecss-from-html');
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['**/*.html', '**/*.js'],
   css: ['city.css'],
   safelist: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track'],
-  extractors: [
-    { 
-      extractor: purgeHTML, extensions: ['html']
-    }
-  ],
 });
 
 module.exports = {
